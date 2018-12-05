4313732fdaa6a499ba9227e68d42bbfef13e550e
mikolaj <mikolaj.bogutczak@o2.pl>
2018-12-05 19:24:47 +0100

    Add game over state

--- BallsBreaker/Assets/Scripts/MoveChilds.cs
+++ BallsBreaker/Assets/Scripts/MoveChilds.cs
@@ -7,9 +7,10 @@ public class MoveChilds : MonoBehaviour {
     public bool canMoveDown = true;
     public float offSet;
     public GameObject Ball;
+    public GameObject BottomPanel;
+    public GameObject GameOver;
 
 	void Start () {
-
     }
 	
 	void Update () {
@@ -17,16 +18,34 @@ public class MoveChilds : MonoBehaviour {
 
     public void MoveDown()
     {
-        if(Ball.transform.position.y < transform.position.y)
+        if (CheckIfChildIsAboveBottomPanel())
         {
             if (canMoveDown)
             {
                 transform.position = transform.position + new Vector3(0, offSet);
-                Debug.Log(transform.position);
-                Debug.Log(Ball.transform.position + "Ball");
-                //Pozycja najnizszego klocka musi byc sprawdzona czy czasem nie jest za nisko
                 canMoveDown = false;
             }
         }   
+        else
+        {
+            Debug.Log("Game Over: ");
+        }
+    }
+
+    private bool CheckIfChildIsAboveBottomPanel()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+
+            var offSetBottomPanel = BottomPanel.transform.GetComponent<RectTransform>().anchorMax.y;
+            Debug.Log("Dolny pasek gora: " + BottomPanel.transform.GetComponent<RectTransform>().anchorMin);
+            Debug.Log("Dolny pasek pozycja" + BottomPanel.transform.position);
+            if ((transform.GetChild(i).transform.position.y + offSet) <= (BottomPanel.transform.position.y + offSetBottomPanel))
+            {
+                GameOver.SetActive(true);
+                return false;
+            }
+        }
+        return true;
     }
 }
