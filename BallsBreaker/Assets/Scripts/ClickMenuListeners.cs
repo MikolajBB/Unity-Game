2dfde2cff918aabe41b22e8f55df0df1b0bb9343
mikolaj <mikolaj.bogutczak@o2.pl>
2019-01-06 18:34:41 +0100

    Add ChooseLevel Panel

--- BallsBreaker/Assets/Scripts/ClickMenuListeners.cs
+++ BallsBreaker/Assets/Scripts/ClickMenuListeners.cs
@@ -9,4 +9,9 @@ public class ClickMenuListeners : MonoBehaviour {
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
---
+++ BallsBreaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject BottomPanel;
+    public GameObject BallCoordinator;
+    public GameObject ChooseLevelPanel;
+    private GameObject currentLevel;
+
+	public void StartGame(GameObject level)
+    {
+        currentLevel = level;
+        ChooseLevelPanel.SetActive(false);
+        StartLeveL(level);
+    }
+
+    void Update()
+    {
+        if (currentLevel != null)
+        {
+            Debug.Log(currentLevel.transform.childCount);
+            if(currentLevel.transform.childCount <= 0)
+            {
+                Debug.Log("Koniec tej planszy");
+            }
+        }
+    }
+
+    void StartLeveL(GameObject level)
+    {
+        level.SetActive(true);
+        BottomPanel.SetActive(true);
+        BallCoordinator.SetActive(true);
+    }
+}
